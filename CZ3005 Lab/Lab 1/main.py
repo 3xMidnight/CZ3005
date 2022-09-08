c1abdad9a0af1d4f4da5d995ac97839f1786b2a7
3xMidnight <74453081+3xMidnight@users.noreply.github.com>
2022-09-08 16:21:28 +0800

    Add files via upload

---
+++ CZ3005 Lab/Lab 1/main.py
@@ -0,0 +1,21 @@
+
+import task1
+import task2
+import task3
+
+
+def main():
+    
+
+
+def task_1():
+
+def task_2():
+
+
+
+def task_3():
+
+
+if __name__ == "__main__":
+    main()
---
+++ CZ3005 Lab/Lab 1/task1.py
@@ -0,0 +1,3 @@
+#Task 1: You will need to solve a relaxed version of the NYC instance where we do not have the energy 
+#constraint. You can use any algorithm we discussed in the lectures. Note that this is equivalent to 
+#solving the shortest path problem. The solution quality of your algorithm will affect the grade.
