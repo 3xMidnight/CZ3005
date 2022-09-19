56f9a95af2d973a556a40491735efcd7410d0a58
shreejaa_03 <79505494+shreejaa03@users.noreply.github.com>
2022-09-19 22:14:44 +0800

    Update main.py

--- CZ3005 Lab/Lab 1/main.py
+++ CZ3005 Lab/Lab 1/main.py
@@ -1,21 +1,17 @@
-
-import task1
+import json
+from task1 import ucs
 import task2
 import task3
 
+g = open("G.json")
+cost = open("Cost.json")
+coord = open("Coord.json")
+dist = open("Dist.json")
+G = json.load(g)
+Cost = json.load(cost)
+Coord = json.load(coord)
+Dist = json.load(dist)
+ucs ("1", "50", G, Dist, Cost)
+with open('task_1_output.txt') as fp:
+    data1 = fp.read()
 
-def main():
-    
-
-
-def task_1():
-
-def task_2():
-
-
-
-def task_3():
-
-
-if __name__ == "__main__":
-    main()
