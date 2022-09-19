c064b40180a256deac9264648dc4764ba3cb637e
3xMidnight <74453081+3xMidnight@users.noreply.github.com>
2022-09-19 23:25:29 +0800

    kj code, reference from github

--- CZ3005 Lab/Lab 1/main.py
+++ CZ3005 Lab/Lab 1/main.py
@@ -1,21 +1,30 @@
 
 import task1
-import task2
-import task3
+from timeit import timeit
+from typing import Tuple
+import json
+
+PERF_SAMPLES = 5000
+
+PathInfo =''
 
 
 def main():
+    G, Cost, Coord, Dist = load_json_files()
+    path1 = task1.ucs('1', '50', Dist, G)
+
+
+def load_json_files():
+    g = open("G.json")
+    cost = open("Cost.json")
+    coord = open("Coord.json")
+    dist = open("Dist.json")
+    G = json.load(g)
+    Cost = json.load(cost)
+    Coord = json.load(coord)
+    Dist = json.load(dist)
+    return G, Cost, Coord, Dist
     
-
-
-def task_1():
-
-def task_2():
-
-
-
-def task_3():
-
 
 if __name__ == "__main__":
     main()
