7201766c846b9deddc53d75876c97ae0d3506d5a
shreejaa_03 <79505494+shreejaa03@users.noreply.github.com>
2022-09-19 22:43:13 +0800

    Update task1.py

--- CZ3005 Lab/Lab 1/task1.py
+++ CZ3005 Lab/Lab 1/task1.py
@@ -1,25 +1,24 @@
 
-def ucs(start, end, G, Distance, Cost):
-    Queue = [[0, 0, -1, start]]
+def ucs(start, end, G, Distance):
+    Queue = [[0, -1, start]]
     Memory = []
     Visited = []
     while (len(Queue) > 0):
         Queue = sorted(Queue)
-        dist, cost, Parent, Node = Queue.pop()
-        Memory.append([distance, cost, Parent, Node])
+        dist,Parent, Node = Queue.pop()
+        Memory.append([dist, Parent, Node])
         dist = -dist
 
         if (Node == end):
             s = end
             for i in range(len(Memory)-1, -1, -1):
-                if Memory[i][3] == Parent:
+                if Memory[i][2] == Parent:
                     s = Parent + "->" + s
-                    Parent = Memory[i][2]
+                    Parent = Memory[i][1]
             f = open("task_1_output.txt", "w")
             f.write("TASK1:\n")
             f.write("Shortest Path:" + s + "\n")
             f.write("Shortest distance: {}".format(dist) + "\n")
-            f.write("Total Energy Cost: {}".format(cost) + "\n")
             return
 
         if Node in Visited:
@@ -27,7 +26,6 @@ def ucs(start, end, G, Distance, Cost):
         for i in range(len(G[Node])):
             if G[Node][i] not in Visited:
                 d = dist + Distance["{}, {}".format(int(Node), int(G[Node][i]))]
-                c = cost + Cost["{}, {}".format(int(Node), int(G[Node][i]))]
-                Queue.append([-d, c, Node, G[Node][i]])
+                Queue.append([-d, Node, G[Node][i]])
         Visited.append(Node)
 
