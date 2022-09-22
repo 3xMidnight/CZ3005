0f0debbff0dd176fc2f6afcbefeee94867f8f003
shreejaa_03 <79505494+shreejaa03@users.noreply.github.com>
2022-09-22 18:33:31 +0800

    Update main.py

--- CZ3005 Lab/Lab 1/main.py
+++ CZ3005 Lab/Lab 1/main.py
@@ -1,17 +1,35 @@
+import task1,task2,task3
+from timeit import timeit
+from typing import Tuple
 import json
-from task1 import ucs
-import task2
-import task3
 
-g = open("G.json")
-cost = open("Cost.json")
-coord = open("Coord.json")
-dist = open("Dist.json")
-G = json.load(g)
-Cost = json.load(cost)
-Coord = json.load(coord)
-Dist = json.load(dist)
-ucs("1", "50", G, Dist)
-with open('task_1_output.txt') as fp:
-    output1 = fp.read()
+PERF_SAMPLES = 5000
+
+PathInfo =''
+EnergyBudget = 287932
+
+
+def main():
+    G, Cost, Coord, Dist = load_json_files()
+    task1.ucs('1', '50', Dist, G)
+
+    task2.ucs('1', '50', EnergyBudget, Dist, G, Cost)
+    
+    task3.astar('1', '50', G, Dist, Cost, EnergyBudget, Coord)
+
+
+def load_json_files():
+    g = open("G.json")
+    cost = open("Cost.json")
+    coord = open("Coord.json")
+    dist = open("Dist.json")
+    G = json.load(g)
+    Cost = json.load(cost)
+    Coord = json.load(coord)
+    Dist = json.load(dist)
+    return G, Cost, Coord, Dist
+    
+
+if __name__ == "__main__":
+    main()
 
