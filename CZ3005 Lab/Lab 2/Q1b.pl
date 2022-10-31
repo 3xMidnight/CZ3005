ba719422fb4112fa5a21123189dfd2ad31121756
3xMidnight <74453081+3xMidnight@users.noreply.github.com>
2022-10-31 20:28:22 +0800

    changes

--- CZ3005 Lab/Lab 2/Q1b.pl
+++ CZ3005 Lab/Lab 2/Q1b.pl
@@ -1,11 +1,11 @@
 company(sumsum).
 company(appy).
 boss(stevey).
-smartphonetech(galacticaS3).
+tech(galacticaS3).
 competitors(sumsum,appy).
 developed(sumsum,galacticaS3).
 stolen(stevey,galacticaS3,sumsum).
 
 rival(X) :- competitors(X,appy).
-business(X) :- smartphonetech(X).
+business(X) :- tech(X).
 unethical(X) :- boss(X). business(Y), company(Z), rival(Z), stolen(X,Y,Z).
