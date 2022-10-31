d0378657baa2fe62d736025baacfc534e13c70a6
3xMidnight <74453081+3xMidnight@users.noreply.github.com>
2022-10-31 20:31:20 +0800

    fullstop

--- CZ3005 Lab/Lab 2/Q1b.pl
+++ CZ3005 Lab/Lab 2/Q1b.pl
@@ -8,4 +8,4 @@ stolen(stevey,galacticaS3,sumsum).
 
 rival(X) :- competitors(X,appy).
 business(X) :- tech(X).
-unethical(X) :- boss(X). business(Y), company(Z), rival(Z), stolen(X,Y,Z).
+unethical(X) :- boss(X), business(Y), company(Z), rival(Z), stolen(X,Y,Z).
